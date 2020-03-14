3b119eb6c0f018a96679742a56e3d31e31567031
dooditrol <dooditrol@bk.ru>
2020-03-15 00:30:00 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+The game "Moving balls in 2D". Used JavaFX SDK.
---
+++ src/balls2d/Main.java
@@ -0,0 +1,32 @@
+package balls2d;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.text.Text;
+
+public class Main extends Application{
+    
+    public static void main(String[] args) {
+        
+        Application.launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+         
+        Text text = new Text("Hello from \"Moving balls in 2D\"!");
+        text.setLayoutY(80);
+        text.setLayoutX(100);
+         
+        Group group = new Group(text);
+         
+        Scene scene = new Scene(group);
+        stage.setScene(scene);
+        stage.setTitle("First Application");
+        stage.setWidth(300);
+        stage.setHeight(250);
+        stage.show();
+    }
+}
